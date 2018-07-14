c34262a7c221b7918112a35c5140395ced93a2cb
pravi-cet <ravi.cet@gmail.com>
2018-07-14 09:46:52 -0400

    Inline style in js

--- src/App.js
+++ src/App.js
@@ -32,13 +32,20 @@ changeNameHandler = (event) => {
   );
 }
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
       <h1>This is a react app</h1>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.changeNameHandler}/>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, "PName")}/>
-      <button onClick={() => this.switchNameHandler('NameAsParam')}>Swich Name</button>
+      <button style={style} onClick={() => this.switchNameHandler('NameAsParam')}>Swich Name</button>
       </div>
     );
   }
