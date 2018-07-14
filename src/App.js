77908acb6edd5f42c4c7029966533b25c1c788a5
pravi-cet <ravi.cet@gmail.com>
2018-07-14 09:43:00 -0400

    Added style with css

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ changeNameHandler = (event) => {
       <h1>This is a react app</h1>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.changeNameHandler}/>
-      <button onClick={() => this.switchNameHandler('NameAsParam')}>Swich Name</button>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, "PName")}/>
+      <button onClick={() => this.switchNameHandler('NameAsParam')}>Swich Name</button>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css';
 const Person = (props) => {
   return (
-    <div>
+    <div className="Person">
     <p onClick={props.click}>I am a {props.name} and I am {props.age}</p>
     <p>{props.children}</p>
     <input type="text" onChange={props.changed} value={props.name}/>
